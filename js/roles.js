c9b0b9471c2004be7d3f8576139ba7d22ce65226
Thangarasu <thangarasu.us@gmail.com>
2025-02-26 12:20:21 +0530

    Updated Datatable changes.

--- js/roles.js
+++ js/roles.js
@@ -7,35 +7,122 @@ $(document).ready(function() {
 			data: { flag: "fetch" },
             dataType: "json",
             success: function (data) {
-                let tbody = "";
-                data.forEach(function (row) {
-					console.log(row)
-                    let statusClass = row.status === "1" ? "badge-success" : "badge-danger";
-					let statusValue = row.status === "1" ? "Active" : "Inactive";
-                    tbody += `<tr>
-                        <td>
-                            <div class="form-check form-check-md">
-                                <input class="form-check-input" type="checkbox">
-                            </div>
-                        </td>
-                        <td>
-                            <h6 class="fw-medium"><a href="#">${row.role_name}</a></h6>
-                        </td>
-                        <td>
-                            <span class="badge ${statusClass} d-inline-flex align-items-center badge-xs">
-                                <i class="ti ti-point-filled me-1"></i>${statusValue}
-                            </span>
-                        </td>
-                        <td>
-                            <div class="action-icon d-inline-flex">
-                                <a href="#" class="me-2 edit-department" data-id="${row.role_id}" data-bs-toggle="modal" data-bs-target="#edit_department"><i class="ti ti-edit"></i></a>
-                                <a href="javascript:void(0);" class="delete-department" data-id="${row.role_id}" data-bs-toggle="modal" data-bs-target="#delete_modal"><i class="ti ti-trash"></i></a>
-                            </div>
-                        </td>
-                    </tr>`;
+                var tableBody = $("#tableRecords tbody");
+                if ($.fn.DataTable.isDataTable("#tableRecords")) {
+                    $("#tableRecords").DataTable().destroy();
+                  }
+                tableBody.empty();
+
+                if (data.length > 0) {
+                    $.each(data, function (index, row) {
+                      let statusClass =
+                        row.status === "1" ? "badge-success" : "badge-danger";
+                      let statusValue = row.status === "1" ? "Active" : "Inactive";
+                      var newRow = `<tr>
+                                      <td>${index + 1}</td>
+                                      <td><h6 class="fw-medium">${row.role_name}</h6></td>
+                                      <td><span class="badge ${statusClass} d-inline-flex align-items-center badge-xs"><i class="ti ti-point-filled me-1"></i>${statusValue}</span></td>
+                                      <td><div class="action-icon d-inline-flex"><a href="#" class="me-2 edit_user" data-id="${row.role_id}" data-bs-toggle="modal" data-bs-target="#add_edit_user"><i class="ti ti-edit"></i></a>
+                                      <a href="#" class="delete-users" data-id="${row.role_id}" data-bs-toggle="modal" data-bs-target=""><i class="fa-solid fa-check-circle"></i></a>
+                                      </div>
+                                      </td>
+                                  </tr>`;
+                      tableBody.append(newRow);
+                    });
+                }
+                var lastSegment = $(location).attr("pathname").split("/").pop();
+
+                table = $("#tableRecords").DataTable({
+                    pageLength: 10,
+                    lengthChange: false,
+                    language: {
+                        search: "",
+                    },
+                    lengthChange: false,
+                    search: false,
+                    dom:
+                        "<'row'<'col-md-6'B><'col-md-6 text-end'f>>" +
+                        "<'row'<'col-12'tr>>" +
+                        "<'datatable-footer'<i><p>>",
+            
+                    buttons: [
+                        {
+                        extend: "excelHtml5",
+                        text: "Export to Excel",
+                        title: lastSegment + " List",
+                        className: "btn btn-success",
+                        exportOptions: {
+                            columns: ":visible",
+                        },
+                        className: "d-none",
+                        },
+                        {
+                        extend: "pdf",
+                        text: "Export to PDF",
+                        title: lastSegment + " List",
+                        className: "buttons-pdf",
+                        exportOptions: {
+                            columns: ":visible",
+                        },
+                        className: "d-none",
+                        },
+                        {
+                        extend: "copy",
+                        text: "Export to copy",
+                        title: lastSegment + " List",
+                        className: "buttons-copy",
+                        exportOptions: {
+                            columns: ":visible",
+                        },
+                        className: "d-none",
+                        },
+                        {
+                        extend: "csv",
+                        text: "Export to csv",
+                        title: lastSegment + " List",
+                        className: "buttons-csv",
+                        exportOptions: {
+                            columns: ":visible",
+                        },
+                        className: "d-none",
+                        },
+                        {
+                        extend: "print",
+                        text: "Export to print",
+                        title: lastSegment + " List",
+                        className: "buttons-print",
+                        exportOptions: {
+                            columns: ":visible",
+                        },
+                        className: "d-none",
+                        },
+                    ],
                 });
 
-                $("tbody").html(tbody);
+                $("#excel_button").on("click", function () {
+                table.button(".buttons-excel").trigger();
+                });
+                $("#pdf_button").on("click", function () {
+                table.button(".buttons-pdf").trigger();
+                });
+                $("#copy_button").on("click", function () {
+                table.button(".buttons-copy").trigger();
+                });
+                $("#csv_button").on("click", function () {
+                table.button(".buttons-csv").trigger();
+                });
+                $("#print_button").on("click", function () {
+                table.button(".buttons-print").trigger();
+                });
+
+                oTable = $("#tableRecords").DataTable();
+                $("#myInputTextField").keyup(function () {
+                oTable.search($(this).val()).draw();
+                });
+                $("#customLengthMenu").on("change", function () {
+                var length = $(this).val();
+                table.page.len(length).draw();
+                });
             },
         });
     }
