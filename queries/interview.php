ca92b2b4bdd8bbed4e73d0c3ffa2af185b0adab0
Nithan <nilanithancse2015@gmail.com>
2025-04-07 17:50:10 +0530

    interview query update

--- queries/interview.php
+++ queries/interview.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['flag'])) {
             $departmentWise = "AND e.department_id = $departmentId";
         }
 
-        $sql = "SELECT c.*,r.job_position FROM `candidates` AS c INNER JOIN recruitment AS r ON c.ticket_request_id = r.ticket_request_id INNER JOIN employees AS e ON e.employee_id = r.raised_by WHERE c.`responce_status` = 1 AND `interview_status` IN (3,4,5,6,7,8,9) $departmentWise ORDER BY c.`candidate_id` DESC"; 
+        $sql = "SELECT c.*,r.job_position FROM recruitment AS r  INNER JOIN `candidates` AS c ON c.ticket_request_id = r.ticket_request_id INNER JOIN employees AS e ON e.employee_id = r.raised_by WHERE r.`responce_status` = 1 AND r.`interview_status` IN (3,4,5,6,7,8,9) $departmentWise ORDER BY r.`candidate_id` DESC"; 
         $result = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
